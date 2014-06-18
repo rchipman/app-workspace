796112fea06171e2e5492bf06633db2bd4ddd065
Rob Chipman <jrchipman1@gmail.com>
2014-06-17 22:18:34 -0400

    Converted ng table stuff to coffeescript

--- webapp/emsite/workspace/js/main.js
+++ webapp/emsite/workspace/js/main.js
@@ -1,30 +1,94 @@
-/* can't see any functional difference between this js and the example */
+var Workspace, em;
 
-var Workspace = angular.module('Workspace', ['ngTable']);
+em = (function() {
+  var unit;
 
-Workspace.controller('collabTable', function($scope, $filter, ngTableParams) {
-	 var data = 
-		  [
-		  {id: '47', project: 'July 2014', collab: 'Cover page Belize', owner: 'Shane Sandefur', approval: '3/5', last: '6/7/14'},
-		  {id: '113', project: 'July 2014', collab: 'Honeybee colony collapse infographic', owner: 'Hilde Schwarzbiene', approval: '7/7', last: '6/4/14'},
-		  {id: '57', project: 'Advertising', collab: 'Antarctica cruise ad 3', owner: 'James Roberson', approval: '1/3', last: '5/31/14'},
-		  {id: '133', project: 'August 2014', collab: 'new TOC layout', owner: 'Bill Brasky', approval: '4/5', last: '6/17/14'}
-		  ]
-	      $scope.tableParams = new ngTableParams({
-	    	  page: 1,				// show first page
-	    	  count: 5,				// show 5 per page
-	    	  sorting: {			// sorting init
-	    		  id: 'asc'
-	    	  }
-	      }, {
-	    	  total: data.length, 	// length of data var
-	    	  getData: function($defer, params) {
-	              // use build-in angular filter
-	              var orderedData = params.sorting() ?
-	                                  $filter('orderBy')(data, params.orderBy()) :
-	                                  data;
-	                                  
-                  $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
-	    	  }    
-	      });
-  });
+  function em() {}
+
+  unit = {};
+
+  em.unit = unit;
+
+  return em;
+
+})();
+
+Workspace = angular.module('Workspace', ['ui.router', 'ngTable']);
+
+Workspace.config;
+
+[
+  '$locationProvider', '$httpProvider', '$stateProvider', '$urlRouterProvider', function($locationProvider, $httpProvider, $stateProvider, $urlRouterProvider) {
+    return $stateProvider.state('app', {
+      abstract: true,
+      template: '<div ui-view></div>'
+    }).state('app.dashboard', {
+      url: '/:test',
+      templateUrl: '/partials/dashboard/dashboard.tpl.html',
+      controller: 'DashboardCtrl'
+    }).state('app.collaboration', {
+      url: '/collaboration/:id',
+      templateUrl: '/partials/collaboration/collaboration-details.tpl.html',
+      controller: 'CollaboartionCtrl'
+    });
+  }
+];
+
+Workspace.controller('DashboardCtrl', [
+  '$scope', function($scope) {
+    $scope.testChangeButton = function(text) {
+      if (!text) {
+        text = 'now test is this!';
+      }
+      $scope.test = text;
+      return em.unit;
+    };
+    return em.unit;
+  }
+]);
+
+Workspace.controller('collabTable', [
+  '$scope', '$filter', 'ngTableParams', function($scope, $filter, ngTableParams) {
+    var data;
+    data = [
+      {
+        id: '47',
+        project: 'July 2014',
+        collab: 'Cover page Belize',
+        owner: 'Shane Sandefur',
+        approval: '3/5',
+        last: '6/7/14'
+      }, {
+        id: '48',
+        project: 'July 2014',
+        collab: 'zCover page Belize',
+        owner: 'Shane Sandefur',
+        approval: '3/5',
+        last: '6/7/14'
+      }, {
+        id: '49',
+        project: 'July 2014',
+        collab: 'aCover page Belize',
+        owner: 'Shane Sandefur',
+        approval: '3/5',
+        last: '6/7/14'
+      }
+    ];
+    $scope.tableParams = new ngTableParams({
+      page: 1,
+      count: 5,
+      sorting: {
+        id: 'asc'
+      }
+    }, {
+      total: data.length,
+      getData: function($defer, params) {
+        var orderedData;
+        orderedData = params.sorting() ? $filter('orderBy')(data, params.orderBy()) : data;
+        $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        return em.unit;
+      }
+    });
+    return em.unit;
+  }
+]);
