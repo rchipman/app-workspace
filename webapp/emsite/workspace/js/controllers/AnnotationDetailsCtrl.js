7b8937c0aa9ceac724f98fc09cc77592e85d7610
Adam Bellows <adam@entermediasoftware.com>
2014-06-26 02:50:54 -0400

    working on event handlers, etc

--- webapp/emsite/workspace/js/controllers/AnnotationDetailsCtrl.js
+++ webapp/emsite/workspace/js/controllers/AnnotationDetailsCtrl.js
@@ -13,47 +13,45 @@ Workspace.controller('AnnotationDetailsCtrl', [
     $scope.eventIndex = 0;
     $scope.annotationAction = null;
     $scope.currentAnnotationGroup = [];
+    $
     $scope.currentAnnotationGroupId = 0;
+    // _.contains(array, entry) -> bool is entry in array
     $scope.fabric.canvas.on('mouse:down', function() {
       if ($scope.annotationAction != null) {
         $timeout.cancel($scope.annotationAction);
       }
       return em.unit;
     });
+
     $scope.fabric.canvas.on('mouse:up', function(e) {
-      console.log(e);
+      console.log("mouse:up -> "+e);
       $scope.annotationAction = $timeout(function() {
         $scope.events.push({
           id: $scope.eventIndex++,
           text: 'Object added!'
         });
+        console.log($scope.fabric.canvas.add);
+        console.log($scope.currentAnnotationGroup);
         $scope.fabric.canvas.add(new fabric.Group($scope.currentAnnotationGroup, {
           top: 150,
           left: 100,
           angle: -10
         }));
-        $scope.currentAnnotationGroup = [];
         $scope.$apply();
+        $scope.currentAnnotationGroup.push();
+        alert("You added an object group!");
         return em.unit;
       }, 3000);
       return em.unit;
     });
     $scope.fabric.canvas.on('object:added', function(obj) {
-      // this logic seems slightly recursive
-      // circles are being placed more often than expected
-      /* this circle stuff is just to debug the event handler
-      var circle;
-      circle = new fabric.Circle({
-        radius: 10,
-        fill: "rgba(" + (Math.floor(Math.random() * 255) + 1) + ", " + (Math.floor(Math.random() * 255) + 1) + ", " + (Math.floor(Math.random() * 255) + 1) + ", 1)",
-        stroke: "rgba(" + (Math.floor(Math.random() * 255) + 1) + ", " + (Math.floor(Math.random() * 255) + 1) + ", " + (Math.floor(Math.random() * 255) + 1) + ", 1)",
-      });
-      console.log(circle);
-      $scope.currentAnnotationGroup.push(circle);
-      */
-      console.log(obj);
-      // alert("You added an object group!");
-      return em.unit;
+        $scope.currentAnnotationGroup.push(new fabric.Circle({
+            radius: Math.floor(Math.random() * 14) + 1,
+            color: 'green'
+        }));
+        console.log("object:added");
+        console.log(obj);
+        return em.unit;
     });
     return em.unit;
   }
