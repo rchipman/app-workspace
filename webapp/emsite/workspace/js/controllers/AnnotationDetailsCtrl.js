5e4b490445aa94120969f9df36948fd2530bdbd0
Rob Chipman <jrchipman1@gmail.com>
2014-06-26 02:47:35 -0400

    Setting up highlights theme

--- webapp/emsite/workspace/js/controllers/AnnotationDetailsCtrl.js
+++ webapp/emsite/workspace/js/controllers/AnnotationDetailsCtrl.js
@@ -1,5 +1,55 @@
 Workspace.controller('AnnotationDetailsCtrl', [
   '$scope', '$stateParams', '$timeout', 'annotationService', 'fabricJsService', function($scope, $stateParams, $timeout, annotationService, fabricJsService) {
+
+    $scope.currentCommentIndex = 3
+    $scope.newCommentText = null
+
+    var comment = {
+        type: 'normal',
+        name: 'Rob',
+        email: md5('jrchipman1@gmail.com'),
+        text: 'This is a comment that some dude left on here. cool.',
+        annotationId: 3,
+        timestamp: moment().fromNow()
+    }
+
+    var comment2 = {
+        type: 'normal',
+        name: 'Chris',
+        email: md5('test@gmail.com'),
+        text: 'Hey, what about the thing on the right here, don\'t forget to, do the stuff.',
+        annotationId: 2,
+        timestamp: moment().subtract('minutes', 30).fromNow()
+    }
+
+    var comment3 = {
+        type: 'normal',
+        name: 'Adam',
+        email: md5('test@gmail.com'),
+        text: 'I dont feel like the sky is as blue as it could be, perhaps we should revisit?',
+        annotationId: 1,
+        timestamp: moment().subtract('days', 1).fromNow()
+    }
+
+    // Highlights UI stuff, to be deleted
+    $scope.comments = [comment,comment2,comment3]
+    $scope.approved = [1,2,3,4]
+    $scope.rejected = [1]
+    $scope.images = [1,2,3,4,5,6]
+
+    $scope.addComment = function () {
+        $scope.comments.unshift({
+            type: 'normal',
+            name: 'Rob',
+            email: md5('jrchipman1@gmail.com'),
+            text: $scope.newCommentText,
+            annotationId: ++$scope.currentCommentIndex,
+            timestamp: moment().fromNow()
+        })
+        $scope.newCommentText = null
+    }
+
+
     var usefulKeys;
     $scope.annotations = [];
     // events attribute holds information about the unique event
